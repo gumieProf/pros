9c6fce6ce04663938ea443fa300a817f57962096
gutsmine <mine>
2019-11-02 12:15:35 +0900

    ADD js json

---
+++ pros/src/pros.js
@@ -0,0 +1,37 @@
+﻿var html = window.document;
+var gebi = function(name, data){ 
+eval("var" + name+"=document.getElementById("+data+")");
+};
+var cli = (function (ele, func, data){
+ele.onclick=function(){
+if(data == null || data == undefined || data==""){
+eval(func+"("+data+")");
+}else{
+eval(func+"()");
+}
+}
+});
+var che = function (ele, func, data){
+data.addEventlistener("change",(event)=>{
+if(data == null || data == undefined || data==""){
+eval(func+"()");
+}else{
+eval(func+"("+data+")")
+}
+});
+};
+var kup = function (data, func, data){
+data.addEventlistener("keyup", event=>{
+
+if(event.isComposing){
+if(data == null || data == undefined || data==""){
+eval(func+"()");
+}else{
+eval(func+"("+data+")");
+  }
+ }
+});
+}
+var inhtm = function(data, text){
+data.innerHTML = text;
+};
