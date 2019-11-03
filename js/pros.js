8c7b849d9310bde946a16d48952ad7f25a2c2ef8
gutsmine <mine>
2019-11-03 18:47:35 +0900

    FIX js

--- js/pros.js
+++ js/pros.js
@@ -6,21 +6,21 @@ var pros = function(){
 this.getid = function(name, data){
 
 eval("var" + name+"=document.getElementById("+data+")");
-};
+}
 this.getclass = function(name, data){
 eval("var" + name+"=document.getElementByClass("+data+")");
 this.getidval = function(name,data){
 eval("var" + name+"=document.getElementById("+data+").value");
-};
+}
 this.getname = function(name, data){
 
 eval("var" + name+"=document.getElementByName("+data+")");
 this.getclassval = function(name, datae){
 eval("var" + name+"=document.getElementByClass("+data+").value");
-};
+}
 this.getnamevalue = function(name, data){
 eval("var" + name+"=document.getElementByName("+data+").value");{ 
-};
+}
 this.loop = function(times, func, arg){
 for(var i=0; i=times; ++i){
 if(arg == null || arg == undefined || arg == ""){
@@ -29,7 +29,7 @@ eval(func+"()");
 eval(func+"("+arg+")");
 }
 }
-};
+}
 this.cli = function (elem, func, arg){
 elem.onclick=function(){
 if(arg == null || arg == undefined || arg ==""){
@@ -38,7 +38,7 @@ eval(func+"()");
 eval(func+"("+arg+")");
 }
 }
-};
+}
 this.cha = function (elem, func, arg){
 elem.addEventlistener("change",(event)=>{
 if(arg == null || arg == undefined || arg ==""){
@@ -46,8 +46,8 @@ eval(func+"()");
 }else{
 eval(func+"("+arg+")")
 }
-});
-};
+}
+}
 this.kup = function (ele, func, data){
 ele.addEventlistener("keyup", event=>{
 
@@ -58,24 +58,24 @@ eval(func+"()");
 eval(func+"("+data+")");
   }
  }
-});
+}
 this.inhtm = function(data, text){
 data.innerHTML = text;
-};
+}
 this.newobj = function(name, data){
 eval("var "+name+"={"+data+"}");
-};
+}
 this.addobj = function(name, data){
 eval(name+".push("+data+")");
-};
+}
 this.newlist = function(name, data){
 eval("var "+name+"=["+data+"];");
-};
+}
 this.addlist = function(name, data){
 eval(name+".push("+data+")")
-};
+}
 this.margevar = function(name, var1, var2){
 eval("var "+name+"="+var1+"+"+var2);
-};
-};
-};
+}
+}
+}
