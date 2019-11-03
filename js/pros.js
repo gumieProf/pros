446a7728670861a2395a65c6099b67fba9297a88
gutsmine <mine>
2019-11-03 19:30:46 +0900

    FIX js

--- js/pros.js
+++ js/pros.js
@@ -1,20 +1,21 @@
 ﻿exports.getid = function(name, data){
 return eval("var" + name+"=document.getElementById("+data+")");
-}
+};
 exports.getclass = function(name, data){
 return eval("var" + name+"=document.getElementByClass("+data+")");
+};
 exports.getidval = function(name,data){
 return eval("var" + name+"=document.getElementById("+data+").value");
-}
+};
 exports.getname = function(name, data){
 return eval("var" + name+"=document.getElementByName("+data+")");
-}
+};
 exports.getclassval = function(name, datae){
 return eval("var" + name+"=document.getElementByClass("+data+").value");
-}
+};
 exports.getnamevalue = function(name, data){
 return eval("var" + name+"=document.getElementByName("+data+").value");
-}
+};
 exports.loop = function(times, func, arg){
 for(var i=0; i=times; ++i){
 if(arg == null || arg == undefined || arg == ""){
@@ -23,7 +24,7 @@ return eval(func+"()");
 return eval(func+"("+arg+")");
   }
  }
-}
+};
 exports.cli = function (elem, func, arg){
 elem.onclick=function(){
 if(arg == null || arg == undefined || arg ==""){
@@ -32,7 +33,7 @@ return eval(func+"()");
 return eval(func+"("+arg+")");
   }
  }
-}
+};
 this.cha = function (elem, func, arg){
 elem.addEventlistener("change",(event)=>{
 if(arg == null || arg == undefined || arg ==""){
@@ -52,22 +53,22 @@ return eval(func+"("+data+")");
    }
   }
  });
-}
+};
 exports.inhtm = function(data, text){
 return data.innerHTML = text;
-}
+};
 exports.newobj = function(name, data){
 return eval("var "+name+"={"+data+"}");
-}
+};
 exports.addobj = function(name, data){
 return eval(name+".push("+data+")");
-}
+};
 exports.newlist = function(name, data){
 return eval("var "+name+"=["+data+"];");
-}
+};
 exports.addlist = function(name, data){
 return eval(name+".push("+data+")");
-}
+};
 exports.margevar = function(name, var1, var2){
 return eval("var "+name+"="+var1+"+"+var2);
- }
+ };
