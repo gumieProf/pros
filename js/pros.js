874dd78b77db049ad9c4a0fc96d68795bf452d0f
gutsmine <mine>
2019-11-04 07:39:57 +0900

    ADD dev FIX js

--- js/pros.js
+++ js/pros.js
@@ -1,22 +1,14 @@
-﻿exports.getid = function(name, data){
+﻿var pros = (){
+ :getid = function(name, data){
 return eval("var" + name+"=document.getElementById("+data+")");
-};
-exports.getclass = function(name, data){
+}
+ :getclass = function(name, data){
 return eval("var" + name+"=document.getElementByClass("+data+")");
 };
-exports.getidval = function(name,data){
-return eval("var" + name+"=document.getElementById("+data+").value");
-};
-exports.getname = function(name, data){
+ :getname = function(name, data){
 return eval("var" + name+"=document.getElementByName("+data+")");
 };
-exports.getclassval = function(name, datae){
-return eval("var" + name+"=document.getElementByClass("+data+").value");
-};
-exports.getnamevalue = function(name, data){
-return eval("var" + name+"=document.getElementByName("+data+").value");
-};
-exports.loop = function(times, func, arg){
+ :loop = function(times, func, arg){
 for(var i=0; i=times; ++i){
 if(arg == null || arg == undefined || arg == ""){
 return eval(func+"()");
@@ -24,8 +16,7 @@ return eval(func+"()");
 return eval(func+"("+arg+")");
   }
  }
-};
-exports.cli = function (elem, func, arg){
+ :click = function (elem, func, arg){
 elem.onclick=function(){
 if(arg == null || arg == undefined || arg ==""){
 return eval(func+"()");
@@ -33,8 +24,7 @@ return eval(func+"()");
 return eval(func+"("+arg+")");
   }
  }
-};
-this.cha = function (elem, func, arg){
+ :change = function (elem, func, arg){
 elem.addEventlistener("change",(event)=>{
 if(arg == null || arg == undefined || arg ==""){
 eval(func+"()");
@@ -42,8 +32,7 @@ eval(func+"()");
 eval(func+"("+arg+")");
   }
  });
-}
-exports.kup = function (ele, func, data){
+ :keyup = function (ele, func, data){
 ele.addEventlistener("keyup", event=>{
 if(event.isComposing){
 if(data == null || data == undefined || data==""){
@@ -53,22 +42,73 @@ return eval(func+"("+data+")");
    }
   }
  });
-};
-exports.inhtm = function(data, text){
+ :inhtm = function(data, text){
 return data.innerHTML = text;
 };
-exports.newobj = function(name, data){
+ :newobj = function(name, data){
 return eval("var "+name+"={"+data+"}");
 };
-exports.addobj = function(name, data){
+ :addobj = function(name, data){
 return eval(name+".push("+data+")");
 };
-exports.newlist = function(name, data){
+ :newlist = function(name, data){
 return eval("var "+name+"=["+data+"];");
 };
-exports.addlist = function(name, data){
+ :addlist = function(name, data){
 return eval(name+".push("+data+")");
-};
-exports.margevar = function(name, var1, var2){
+ :margevar = function(name, var1, var2){
 return eval("var "+name+"="+var1+"+"+var2);
  };
+};
+
+ exports.getid = function(name, data){
+new pros();
+return pros.getid(name, data);
+};
+ exports.getclass = function(name, data){
+new pros();
+return pros.getclass(name, data);};
+ exports.getname = function(name, data){
+new pros();
+return pros.getname(name, data);
+};
+ exports.loop = function(times, func, arg){
+new pros();
+return pros.loop(times, func, arg);
+};
+exports.click = function (elem, func, arg){
+new pros();
+return pros.getid(elem, func, arg);
+};
+exports.change = function (elem, func, arg){
+new pros();
+return pros.change(elem, func, arg);
+};
+ exports.keyup = function (ele, func, data){
+new pros();
+return pros.keyup(elem, func, arg);
+};
+exports.inhtm = function(data, text){
+new pros();
+return pros.inhtml(data, text);
+};
+exports.newobj = function(name, data){
+new pros();
+return pros.newobj(name, data);
+};
+exports.addobj = function(name, data){
+new pros();
+return pros.addobj(name, data);
+};
+exports.newlist = function(name, data){
+new pros();
+return pros.newlist(name, data);
+};
+exports.addlist = function(name, data){
+new pros();
+return pros.addlist(name, data);
+};
+exports.margevar = function(name, var1, var2){
+new pros();
+return pros.margevar(name, var1, var2);
+};
