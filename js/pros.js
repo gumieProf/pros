2bb9aa745ff868ba7220b17728815e2b38a8ef84
gutsmine <mine>
2019-11-03 12:04:28 +0900

    FIX package

--- js/pros.js
+++ js/pros.js
@@ -1,43 +1,37 @@
-﻿module.exports = (function() {
+﻿module.exports = (function(data1, data2, data3) {
 var html = window.document;
-var getid = function(name, data){ 
-eval("var" + name+"=document.getElementById("+data+")");
-};
-var getclass = function(name, data){ 
-eval("var" + name+"=document.getElementByClass("+data+")");
-};
-var getidval = function(name, data){ 
-eval("var" + name+"=document.getElementById("+data+").value");
-};
-var getname = function(name, data){ 
-eval("var" + name+"=document.getElementByName("+data+")");
-};
-var getclassval = function(name, data){ 
-eval("var" + name+"=document.getElementByClass("+data+").value");
-};
+var getid = eval("var" + data1+"=document.getElementById("+data2+")");
+var getclass = eval("var" + data1+"=document.getElementByClass("+data2+")");
+var getidval = eval("var" + data1+"=document.getElementById("+data2+").value");
+var getname = eval("var" + data1+"=document.getElementByName("+data2+")");
+var getclassval = eval("var" + data1+"=document.getElementByClass("+data2+").value");
 var getnamevalue = function(name, data){ 
 eval("var" + name+"=document.getElementByName("+data+").value");
 };
-var loop = function(times, func){
-for(var i=0; i=times; ++i){
-eval(func+"()");
+var loop = function(){
+for(var i=0; i=data1; ++i){
+if(data3 == null || data3 == undefined || data3 == ""){
+eval(data2+"()");
+}else{
+eval(data2+"("+data3+")");
+}
 }
 };
-var cli = (function (ele, func, data){
-ele.onclick=function(){
-if(data == null || data == undefined || data==""){
-eval(func+"("+data+")");
+var cli = function (){
+data1.onclick=function(){
+if(data3 == null || data3 == undefined || data3 ==""){
+eval(data2+"()");
 }else{
-eval(func+"()");
+eval(data2+"("+data3+")");
 }
 }
-});
-var cha = function (ele, func, data){
-data.addEventlistener("change",(event)=>{
-if(data == null || data == undefined || data==""){
-eval(func+"()");
+};
+var cha = function (){
+data1.addEventlistener("change",(event)=>{
+if(data3 == null || data3 == undefined || data3 ==""){
+eval(data2+"()");
 }else{
-eval(func+"("+data+")")
+eval(data2+"("+data3+")")
 }
 });
 };
